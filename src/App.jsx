8ed2dd0301618a605713fc9cf0afad7dc9c8cad5
sameh issa <s11925968@gmail.com>
2023-10-31 08:29:28 +0200

    task5

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter,Route,Routes} from "react-router-dom";
 import Product from './compount/products/Product.jsx';
 import Navbar from './compount/navbar/Navbar.jsx';
 import Footer from './compount/footer/Footer.jsx';
-import Home from './compount/home/Home.jsx';
+import HomePage from './compount/home/HomePage.jsx';
 import PageNotFound from './compount/pagenotfound/PageNotFound.jsx';
 import Resturent from './compount/resturent/Resturent.jsx';
 export default function App() {
@@ -11,7 +11,7 @@ export default function App() {
     <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route path="/" element={<Home />}/>
+      <Route path="/" element={<HomePage />}/>
       <Route path="*" element={<PageNotFound />}/>
       <Route path="/Product" element={<Product />}/>
       <Route path="/Resturent" element={<Resturent />}/>
